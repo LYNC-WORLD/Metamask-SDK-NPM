eaf3f569a2d2ec8b7399d07dd26307e33a51853b
Shanu Joshi <42548654+shanu12joshi@users.noreply.github.com>
2023-05-16 15:36:49 +0530

    Update constantsValues.js

--- src/constantsValues.js
+++ src/constantsValues.js
@@ -1,4 +1,46 @@
 const constantsValues = {
+  "0x1": {
+    chainID: "0x1",
+    blockExplorer: "https://etherscan.io/address/",
+    ChainName: "Ethereum Mainnet",
+    slug: "ethereum",
+  },
+  "0x89": {
+    chainID: "0x89",
+    blockExplorer: "https://polygonscan.com/address/",
+    ChainName: "Polygon Mainnet",
+    slug: "polygon",
+  },
+  "0x250": {
+    chainID: "0x250",
+    blockExplorer: "https://astar.network/address/",
+    ChainName: "Astar Mainnet",
+    slug: "astar",
+  },
+  "0xa86a": {
+    chainID: "0xa86a",
+    blockExplorer: "https://subnets.avax.network/address/",
+    ChainName: "Avalanche Mainnet",
+    slug: "avalanche",
+  },
+  "0x42": {
+    chainID: "0x42",
+    blockExplorer: "https://www.oklink.com/oktc/address/",
+    ChainName: "OKC Mainnet",
+    slug: "okc",
+  },
+  "0xa4b1": {
+    chainID: "0xa4b1",
+    blockExplorer: "https://explorer.arbitrum.io/address/",
+    ChainName: "Arbitrum One Mainnet",
+    slug: "arbitrum",
+  },
+  "0x144": {
+    chainID: "0x144",
+    blockExplorer: "https://explorer.zksync.io/address/",
+    ChainName: "zkSync Mainnet",
+    slug: "zkSync",
+  },
   "0x13881": {
     chainID: "0x13881",
     blockExplorer: "https://mumbai.polygonscan.com/address/",
@@ -12,41 +54,17 @@ const constantsValues = {
     ChainName: "Polygon Zkevm Testnet",
     slug: "polygonzkEVMTestnet",
   },
-  "0x89": {
-    chainID: "0x89",
-    blockExplorer: "https://polygonscan.com/address/",
-    ChainName: "Polygon Mainnet",
-    slug: "polygon",
-  },
   "0x51": {
     chainID: "0x51",
     blockExplorer: "https://blockscout.com/shibuya/address/",
     ChainName: "Astar Shibuya Testnet",
     slug: "shibuya",
   },
-  "0x1": {
-    chainID: "0x1",
-    blockExplorer: "https://etherscan.io/address/",
-    ChainName: "Ethereum Mainnet",
-    slug: "ethereum",
-  },
   "0x5": {
     chainID: "0x5",
     blockExplorer: "https://goerli.etherscan.io/address/",
     ChainName: "Eth Goerli testnet",
     slug: "goerli",
-  },
-  "0x250": {
-    chainID: "0x250",
-    blockExplorer: "https://astar.network/address/",
-    ChainName: "Astar Mainnet",
-    slug: "astar",
-  },
-  "0x144": {
-    chainID: "0x144",
-    blockExplorer: "https://explorer.zksync.io/address/",
-    ChainName: "zkSync Mainnet",
-    slug: "zkSync",
   },
   "0x118": {
     chainID: "0x118",
@@ -60,30 +78,12 @@ const constantsValues = {
     ChainName: "Mantle Testnet",
     slug: "MantleTestnet",
   },
-  "0xa4b1": {
-    chainID: "0xa4b1",
-    blockExplorer: "https://explorer.arbitrum.io/address/",
-    ChainName: "Arbitrum One Mainnet",
-    slug: "arbitrum",
-  },
   "0x66eed": {
     chainID: "0x66eed",
     blockExplorer: "https://goerli.arbiscan.io/address/",
     ChainName: "Arbitrum Goerli Testnet",
     slug: "arbitrumGoerli",
     rpcURL: "https://goerli-rollup.arbitrum.io/rpc",
-  },
-  "0x42": {
-    chainID: "0x42",
-    blockExplorer: "https://www.oklink.com/oktc/address/",
-    ChainName: "OKC Mainnet",
-    slug: "okc",
-  },
-  "0xa86a": {
-    chainID: "0xa86a",
-    blockExplorer: "https://subnets.avax.network/address/",
-    ChainName: "Avalanche mainnet",
-    slug: "avalanche",
   },
   "0xa869": {
     chainID: "0xa869",
